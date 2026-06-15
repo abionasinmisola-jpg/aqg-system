31b6162aae9c480b70d5f50a5ac818555bf9b57c
Sinmi Abiona <abionasinmisola@gmai.com>
2026-06-15 11:58:24 +0100

    Force consistent HF cache path for build and runtime

--- download_model.py
+++ download_model.py
@@ -1,3 +1,6 @@
-﻿from sentence_transformers import SentenceTransformer
+﻿import os
+os.environ["HF_HOME"] = "/app/.cache/huggingface"
+
+from sentence_transformers import SentenceTransformer
 SentenceTransformer('all-MiniLM-L6-v2')
 print('Model downloaded successfully')
